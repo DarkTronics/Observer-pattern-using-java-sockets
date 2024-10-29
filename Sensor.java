06f19e4bc6b07341b476022f5b962e7a9f19345c
Xinwei Guo <xguo50@binghamton.edu>
2024-10-29 14:56:08 -0400

    updating sensor.java

--- Sensor.java
+++ Sensor.java
@@ -10,6 +10,24 @@ public class Sensor {
         if(args.length != 4) { 
             System.out.println("Usage is: " + "java Sensor <type> <initial value> <host name> <port number>"); 
             System.exit(1); 
-         } 
+        }
+        int type = Integer.parseInt(args[0]);
+        int value = Integer.parseInt(args[1]);
+        String hostName = args[2];
+        int portNumber = Integer.parseInt(args[3]);
+
+        if(type < Observable.TEMP_ZONE1 || type > Observable.HUMIDITY_ZONE2) { 
+            System.out.println("The simulations only works with 4 different types 0 through 3"); 
+            System.exit(1); 
+        } 
+
+        try (Socket deviceSocket = new Socket(hostName, portNumber); 
+        DataOutputStream out = new DataOutputStream(deviceSocket.getOutputStream()); 
+        BufferedReader in = new BufferedReader(new InputStreamReader(deviceSocket.getInputStream())))  { 
+        // we will describe this code below 
+        } catch (UnknownHostException e) { 
+            e.printStackTrace(); 
+            } catch (IOException e) {  e.printStackTrace(); 
+        }
     }
 }
